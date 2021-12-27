7a4e01357a4055f3b6ca0b7d6cb9483cc2386486
bzhang222 <bzhang222@gmail.com>
2021-12-27 14:26:06 -0800

    Add files via upload

---
+++ hw1/city.cpp
@@ -0,0 +1,52 @@
+// city.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include <stdio.h>
+
+typedef struct
+{
+    int id;
+    char name[128];
+    char countryCode[4];
+    char district[32];
+    long population;
+} city_t;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    FILE* fp = fopen("city.csv", "r");
+    char buf[1024];
+    int count = 0;
+    city_t c[1024];
+
+    while (!feof(fp))
+    {
+        if (fgets(buf, 1024, fp))
+        {
+            char *ids = strtok(buf, ",");
+            char* names = strtok(NULL, ",");
+            char* code = strtok(NULL, ",");
+            char* dist = strtok(NULL, ",");
+            char* pops = strtok(NULL, ",");
+            c[count].id = atoi(ids);
+            strcpy(c[count].name, names);
+            strcpy(c[count].countryCode, code);
+            strcpy(c[count].district, dist);
+            c[count].population = atoi(pops);
+            count++;
+        }
+
+    }
+    fclose(fp);
+    fp = fopen("city.bin", "wb");
+    for (int i=1; i < count; ++i)
+    {
+        fwrite(&c[i], sizeof(city_t), 1, fp);
+    }
+    fclose(fp);
+}
+
+ 
---
+++ hw1/readme.txt
@@ -0,0 +1,10 @@
+1 install c on windows VS
+2 create a c windows project
+3 use 	fopen with "rb" to read binary file city.bin
+
+	use fseek to SEEK to position of city ID 15
+
+	use fread to read CITY 15's struct
+
+	print CITY 15's info
+
