782f5ad8ff858be9cc5fd83e51ca1cdb25492aa9
bzhang222 <bzhang222@gmail.com>
2022-01-02 00:32:11 -0800

    Update list.cpp

--- hw6/list.cpp
+++ hw6/list.cpp
@@ -63,7 +63,8 @@ int main()
 
 /*
 implement a MergeList function which takes in two sorted linked-list,  they 
-are sorted in ascending order of population,  the new function merge them into 
-a sorted list 
+are sorted in descending order of population,  the new function merge them into 
+a sorted list. Note that the item will be in the reverse order of the insertion, 
+since they are inserted ascendingly, the result list are in descending order.
 
 */
